c6665a63ae315919493123e6999d4ec678b63fda
Dorian Vinai <dorian@vinai.dev>
2025-07-22 20:47:52 +0200

    Changed color for root prompt

--- prompt.zsh
+++ prompt.zsh
@@ -17,7 +17,7 @@ dash_line() {
 }
 add-zsh-hook -Uz precmd dash_line
 if [[ $UID == 0 || $EUID == 0 ]]; then
-    export PROMPT=$'%n@%m:%1~%(?..%{\e[31m%}[%?]%{\e[39m%})%{\e[95m%}%#%{\e[39m%} '
+    export PROMPT=$'%n@%m:%1~%(?..%{\e[31m%}[%?]%{\e[39m%})%{\e[91m%}%#%{\e[39m%} '
 else
     export PROMPT=$'%n@%m:%1~%(?..%{\e[31m%}[%?]%{\e[39m%})%{\e[92m%}\$%{\e[39m%} '
 fi
