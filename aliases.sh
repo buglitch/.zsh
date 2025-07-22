8ba6bbbcefa1f97ba3483c0691ae6e7542a78a07
Dorian Vinai <dorian@vinai.dev>
2025-07-22 23:15:28 +0200

    Add yazi wrapper

--- aliases.sh
+++ aliases.sh
@@ -64,3 +64,12 @@ if [ ! -z ${VIMRUNTIME} ]; then
 
     unset -f vim_alias
 fi
+
+# wrappers
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
