bb1c2f6d02c4992e02a94a8d18354ada75562d28
Dorian Vinai <dorian@vinai.dev>
2025-07-23 11:12:51 +0200

    Changed dollar for lambda in prompt

--- prompt.zsh
+++ prompt.zsh
@@ -5,11 +5,11 @@ dash_var=0
 dash_line() {
    ret=$?
    var=$history[$((HISTCMD - 1))]
-   if [ $dash_var -eq 1 ] && [ "$var" != "clear" ]; then
+   if [ $dash_var -eq 1 ] && [ "$var" != "clear" ] && [ "$var" != "reset" ]; then
        if [ $ret -eq 0 ]; then
-           echo -e "\e[32m${(r:$COLUMNS::-:)}\e[39m"
+           echo -e "\e[32m${(r:$COLUMNS-3::-:)} 󰄬 \e[39m"
        else
-           echo -e "\e[35m${(r:$COLUMNS::-:)}\e[39m"
+           echo -e "\e[31m${(r:$COLUMNS-3::-:)} 󰅖 \e[39m"
        fi
    else
        dash_var=1
@@ -17,7 +17,7 @@ dash_line() {
 }
 add-zsh-hook -Uz precmd dash_line
 if [[ $UID == 0 || $EUID == 0 ]]; then
-    export PROMPT=$'%n@%m:%1~%(?..%{\e[31m%}[%?]%{\e[39m%})%{\e[91m%}%#%{\e[39m%} '
+    export PROMPT=$'%n@%m:%1~%(?..%{\e[31m%}[%?]%{\e[39m%})%{\e[95m%}%#%{\e[39m%} '
 else
-    export PROMPT=$'%n@%m:%1~%(?..%{\e[31m%}[%?]%{\e[39m%})%{\e[92m%}\$%{\e[39m%} '
+    export PROMPT=$'%n@%m:%1~%(?..%{\e[31m%}[%?]%{\e[39m%})%{\e[92m%}λ%{\e[39m%} '
 fi
