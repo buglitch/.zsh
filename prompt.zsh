4596d9650e9f7809cab2c22eb43c70733f8384a1
Dorian Vinai <dorian@vinai.dev>
2025-07-24 21:06:59 +0200

    Added execution time in prompt

--- prompt.zsh
+++ prompt.zsh
@@ -2,29 +2,45 @@
 export PROMPT='%n@%m:%1~\$ '
 autoload -Uz add-zsh-hook
 dash_var=0
+
+function timer() {
+  timer=${timer:-$SECONDS}
+}
+
 dash_line() {
     local ret=${?}
     local var=$history[$((HISTCMD - 1))]
 
     if [[ ($dash_var -eq 2) || ($dash_var -eq 1 && "$var" != "clear" && "$var" != "reset") ]]; then
+        if [ $timer ]; then
+            local timer_show=$(($SECONDS - $timer))
+        fi
+
+        local git_branch="$(git_current_branch)"
         if [ $ret -eq 0 ]; then
-            local git_branch="$(git_current_branch)"
             if [ -z "${git_branch}" ]; then
                 local prefix=""
             else
                 local prefix="󰊢 ${git_branch}$(parse_git_dirty) "
             fi
-            local suffix=" 󰄬 "
+            if [ "${timer_show}" -lt 2 ]; then
+                local suffix=" 󰄬 "
+            else
+                local suffix=" (${timer_show}s) 󰄬 "
+            fi
             local padding=$((${#suffix} + ${#prefix}))
             echo -e "\e[32m${prefix}${(r:$COLUMNS-${padding}::-:)}${suffix}\e[39m"
         else
-            local git_branch="$(git_current_branch)"
             if [ -z "${git_branch}" ]; then
                 local prefix="[${ret}] "
             else
                 local prefix="[${ret}] 󰊢 ${git_branch}$(parse_git_dirty) "
             fi
-            local suffix=" 󰅖 "
+            if [ "${timer_show}" -lt 2 ]; then
+                local suffix=" 󰅖 "
+            else
+                local suffix=" (${timer_show}s) 󰅖 "
+            fi
             local padding=$((${#suffix} + ${#prefix}))
             echo -e "\e[31m${prefix}${(r:$COLUMNS-${padding}::-:)}${suffix}\e[39m"
         fi
@@ -36,7 +52,9 @@ dash_line() {
     else
         dash_var=2
     fi
+    unset timer
 }
+add-zsh-hook -Uz preexec timer
 add-zsh-hook -Uz precmd dash_line
 if [[ $UID == 0 || $EUID == 0 ]]; then
     export PROMPT=$'%n@%m:%1~%(?..%{\e[31m%}%{\e[39m%})%{\e[95m%}%#%{\e[39m%} '
